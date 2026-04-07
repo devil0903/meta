e8765834e752f1e0910fd785e9a26569043f20a4
devil0903 <jitusingh611@gmail.com>
2026-04-08 01:06:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,17 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-VERIFY_TOKEN = "mytoken123"
+VERIFY_TOKEN = "12345"
 
-@app.route('/webhook', methods=['GET', 'POST'])
+@app.route('/')
+def home():
+    return "OK"
+
+@app.route('/webhook', methods=['GET'])
 def webhook():
-    if request.method == "GET":
-        mode = request.args.get("hub.mode")
-        token = request.args.get("hub.verify_token")
-        challenge = request.args.get("hub.challenge")
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
 
-        if mode == "subscribe" and token == VERIFY_TOKEN:
-            return challenge, 200
-        else:
-            return "Forbidden", 403
-
-    if request.method == "POST":
-        print("📩 Data:", request.json)
-        return "EVENT_RECEIVED", 200
-
-    return "OK", 200
+    if token == VERIFY_TOKEN:
+        return challenge
+    return "Error"
